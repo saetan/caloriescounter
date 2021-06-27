afd233aab75ab279ade844b6f0ece5ab498e532e
saetan <saetan@live.com>
2021-06-27 20:35:46 +0800

    tried out v-model two way binding

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 var app = new Vue({
- el: '#app',
- data: {
- message: 'Hello from Getting to Know Vue.js!'
- }
+    el: '#app',
+    data: {
+        proteins: [{
+            id: 1,
+            type: "chicken",
+            proteinsPer100g: 20,
+            calories: 200
+        },
+        {
+            id: 2,
+            type: "beef",
+            proteinsPer100g: 100,
+            calories: 2000
+        }
+        ],
+        selections: [{
+            id: 1,
+            type: "uwu"
+        }
+        ],
+        message: "uwu"
+    },
+    template:
+        `<div>
+        <h1>{{message}}</h1>
+        <input type= "text" v-model="message">
+        </div>`
 });
